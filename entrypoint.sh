29c289593fc2c3fe759a2621e01ebf770135c9eb
yunxiu <shiruixin.srx@alibaba-inc.com>
2023-08-02 14:03:45 +0800

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export ALIBABA_CLOUD_ACCESS_KEY_SECRET=$INPUT_ACCESS_KEY_SECRET
 
 
 for file in $INPUT_TEMPLATES; do
-  python iact3.py test run -t $file -c 'iact3-config/'.$file
+  python /iact3.py test run -t $file -c 'iact3-config/'.$file
 done
 
 
