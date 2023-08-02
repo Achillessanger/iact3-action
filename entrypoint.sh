75c93c2eab8a359bf5634b1cff21d579dd012caf
yunxiu <shiruixin.srx@alibaba-inc.com>
2023-08-02 15:37:53 +0800

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,17 @@ for file in $INPUT_TEMPLATES; do
   then
     continue
   fi
-  python /iact3.py test run -t $file -c iact3-config/${file%.*}.iact3.yml
+
+  if python /iact3.py test run -t $file -c iact3-config/${file%.*}.iact3.yml | grep -q 'StackValidationFailed'; then
+    exit 1
+  else
+    exit 0
+  fi
+
+#  python /iact3.py test run -t $file -c iact3-config/${file%.*}.iact3.yml
 done
 
-echo $?":code"
+#echo $?":code"
 
 
 #exit 0
