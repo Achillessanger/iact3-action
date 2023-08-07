a908b6904b476ff9d0ba12cfe55543572f0cb630
yunxiu <shiruixin.srx@alibaba-inc.com>
2023-08-07 15:00:33 +0800

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,10 +28,11 @@ for file in $INPUT_TEMPLATES; do
     fi
   else
     python /iact3.py validate -t $file  >> output.txt 2>&1
+    cat output.txt
     if not grep -q "LegalTemplate" output.txt; then
       pass_test=0
     fi
-
+    rm -rf output.txt
   fi
 done
 
